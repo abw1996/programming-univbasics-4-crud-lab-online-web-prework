74b633c6d6c7c9d4be095ad8bdaefa9862d9e760
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-07-21 19:03:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -36,7 +36,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  array[array.length]
+  val = array.length 
+  array[val]
 end
 
 def update_element_from_index(array, index_number, element)
